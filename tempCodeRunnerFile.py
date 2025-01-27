ef1d1806584f64f3e04279bae7ce448c301308a4
Shivam <Shivamkumar188132@gmail.com>
2025-01-27 15:45:56 +0530

    27-01-2025 3:45 P.M trying to visualise the artist count in the artist column

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
- for i in range(numRows):
-        rows= generate_coffecients(i)
-        matrix.append(rows)
-    return matrix
--- testings.py
+++ testings.py
@@ -1,33 +1,25 @@
-def calc(nums):
-    low = float('+inf')
-    for x in nums:
-        if x < low:
-            low = x
+def my_fc(nums):
+        b_i = float('-inf')
+        for i in range(-1,-len(nums),-1):
+            if nums[i-1] < nums[i]:
+                b_i = i-1
+                
+                break
+        if b_i == float('-inf'):
+                nums.reverse()
+                return nums
+        # b_i = -5
+        # b_i = -3
+        for i in range(-1, b_i, -1):
+            if nums[i] > nums[b_i]:
+           # [2,2,4,5,3,0,0] 
+                nums[b_i], nums[i] =  nums[i], nums[b_i]
+            b = nums
+            break
+            # [3,2,1]
+        # [2,2,4,5,3,0,0]
+        nums[b_i+1:] = reversed(nums[b_i+1:])
 
-    high = float('-inf')
-    for x in nums:
-        if x > high:
-            high = x
-            
-    return low, high
-    
-
-# a=calc([1,2,3, 5, 4,7])
-# print(a)
-nums=[1,2,3, 5, 3,4]
-br_i = 0
-smaller = []
-for i in range(-1,-len(nums)-1,-1):
-    print(i, nums[i])
-    if nums[i-1]<nums[i]:
-        smaller.append(nums[i])
-        br_i = nums[i-1]
-        nums[i-1] = nums[i]
-        nums[i] = 
-        break
-# print(nums)
-        
-
-
-
-    
+        return nums, b_i, b
+a = my_fc([2,3,1])
+print(a)
