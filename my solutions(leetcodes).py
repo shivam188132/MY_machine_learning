298cc1b6cf853bd620a7b539899fa7531be86b3f
Shivam <Shivamkumar188132@gmail.com>
2025-02-08 16:58:21 +0530

    4:57 P.M dsa kanden's algorithm

--- my solutions(leetcodes).py
+++ my solutions(leetcodes).py
@@ -1,7 +1,15 @@
-import pandas as pd
-
-# Read the Excel file
-df = pd.read_excel("C:\Users\shiva\Downloads\Amazon Sale Report.xlsx", engine='openpyxl')  # Ensure openpyxl is installed
-
-# Convert to CSV
-df.to_csv("Amazon report.csv", index=False, encoding='utf-8')
+# [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+        #    👆
+def max_subarray_sum_brute_force(arr):
+    n = len(arr)
+    max_sum = float('-inf')
+    
+    for i in range(n): #i = (0,9) i =0,1,2,3
+        for j in range(i, n): # (3,9) j=3
+            curr_sum = 0
+            for k in range(i, j + 1): #(3,4) k=3
+                curr_sum += arr[k] # 4
+                                   # 2
+            max_sum = max(max_sum, curr_sum) #(4, __) --> -2,-1,-1,0,0,1,2,2,2,2,2,2,2,3,4,4,4,4,4,4,4,4,4
+    
+    return max_sum
