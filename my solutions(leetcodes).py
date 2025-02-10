24a5d8ff719c4a6969b16a2d539a184b3f11beff
Shivam <Shivamkumar188132@gmail.com>
2025-02-10 10:56:15 +0530

    10-02-2025 10:55 A.M CCA(amzon dataset)

--- my solutions(leetcodes).py
+++ my solutions(leetcodes).py
@@ -1,15 +1,16 @@
 # [-2,      1,      -3,     4,      -1,     2,      1,      -5,     4]
-#                   🔹                                              👆
-def max_subarray_sum_brute_force(arr):
-    n = len(arr)
+
+#[-2,1,-3,4,-1,2,1,-5,4]
+def max_subarray_sum_better(arr):
+    n = len(arr) # 9
     max_sum = float('-inf')
     
-    for i in range(n): #i = (0,9) i =0,1,2,3
-        for j in range(i, n): # (3,9) j=3
-            curr_sum = 0
-            for k in range(i, j + 1): #(3,4) k=3
-                curr_sum += arr[k] # 4
-                                   # 2
-            max_sum = max(max_sum, curr_sum) #(4, __) --> -2,-1,-1,0,0,1,2,2,2,2,2,2,2,3,4,4,4,4,4,4,4,4,4
+    for i in range(n): # i=0
+        curr_sum = 0
+        for j in range(i, n): # (0,9) j=0,1,2
+            curr_sum += arr[j]  # -2+1-3,4,-1
+            max_sum = max(max_sum, curr_sum) # -2,-1,-5,-1,
     
     return max_sum
+
+print(max_subarray_sum_better([-2,1,-3,4,-1,2,1,-5,4]))
