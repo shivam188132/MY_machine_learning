d0b604588b50b5b4c6d2142a34fc4054f00fec93
Shivam <Shivamkumar188132@gmail.com>
2025-02-17 17:12:59 +0530

    17-02-2024 5:04 P.M applied pandas and visuliased the effect of applying mean/median filling among the distribution of dataset

--- my solutions(leetcodes).py
+++ my solutions(leetcodes).py
@@ -1,19 +1,5 @@
-# [-2,      1,      -3,     4,      -1,     2,      1,      -5,     4]
 
-#[-2,1,-3,4,-1,2,1,-5,4]
-def max_subarray_sum_better(arr):
-    n = len(arr) # 9
-    max_sum = float('-inf')
-    
-    for i in range(n): # i=0
-        curr_sum = 0
-        for j in range(i, n): # (0,9) j=0,1,2
-            curr_sum += arr[j]  # -2+1-3,4,-1
-            max_sum = max(max_sum, curr_sum) # -2,-1,-5,-1,
-    
-    return max_sum
+my_tuple = (1, 2, 3, "hello", 5.5)
 
-# print(max_subarray_sum_better([-2,1,-3,4,-1,2,1,-5,4]))
-
-a = "a"
-print(a+1)
+for item in my_tuple:
+    print(item)  
